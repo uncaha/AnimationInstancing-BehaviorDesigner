6cdbd9d83c1607b0e4fba53b3876f49fe1b8c990
uncaha <cczxntken@163.com>
2020-06-02 16:17:25 +0800

    add

---
+++ Assets/Ani+BehaviorDesigner/LitAnimaion/test/TestBehaviorDesigner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+public class TestBehaviorDesigner : MonoBehaviour
+{
+    public BehaviorTree tree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var ttask = tree.FindTaskWithName("testtask");
+        //tree.StartTaskCoroutine(ttask,"testmethod");
+        tree.EnableBehavior();
+        var ttask = tree.FindTaskWithName("testtask");
+        Debug.Log(ttask);
+        tree.StartTaskCoroutine(ttask,"testmethod");
+    }
+    public void testmethod()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
